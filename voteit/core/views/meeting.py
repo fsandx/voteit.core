e292de907b47451ca9d966d71ae12cbe23e74d9f
Fredrik Nilson <fredrik@betahaus.net>
2012-06-01 16:19:24 +0200

    first version of participants import

--- voteit/core/views/meeting.py
+++ voteit/core/views/meeting.py
@@ -1,5 +1,9 @@
+import csv
 import urllib
 import json
+import random
+
+from StringIO import StringIO
 
 import colander
 from deform import Form
@@ -435,4 +439,73 @@ class MeetingView(BaseView):
             agenda_items.append(self.api.resolve_catalog_docid(docid))
 
         self.response['agenda_items'] = agenda_items
+        return self.response
+
+    @view_config(name="add_participants", context=IMeeting, renderer="templates/base_edit.pt", permission=security.MANAGE_GROUPS)
+    def add_participants(self):
+        """ Add participants to this meeting.
+            Renders a form where you can paste a csv with the users and select which roles they
+            should have once they register. When the form is submitted, a list of userids and 
+            passwords is displayed 
+        """
+        self.response['title'] = _(u"Add meeting participants")
+
+        post = self.request.POST
+        if 'cancel' in post:
+            self.api.flash_messages.add(_(u"Canceled"))
+            url = resource_url(self.context, self.request)
+            return HTTPFound(location=url)
+
+        schema = createSchema('AddParticipantsSchema').bind(context=self.context, request=self.request, api=self.api)
+        add_csrf_token(self.context, self.request, schema)
+
+        form = Form(schema, buttons=(button_add, button_cancel))
+        self.api.register_form_resources(form)
+
+        if 'add' in post:
+            controls = post.items()
+            try:
+                appstruct = form.validate(controls)
+            except ValidationFailure, e:
+                self.response['form'] = e.render()
+                return self.response
+            
+            roles = appstruct['roles']
+            participants = csv.reader(StringIO(appstruct['csv']), delimiter=';', quotechar='"')
+
+            output = []
+            participant_count = 0
+            for row in participants:
+                appstruct = {}
+                userid = row[0]
+                appstruct['password'] = row[1]
+                if not appstruct['password']:
+                    chars = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789#$%&'
+                    appstruct['password'] = row[1] = ''.join(random.choice(chars) for x in range(10))
+                appstruct['email'] = row[2]
+                appstruct['first_name'] = row[3]
+                appstruct['last_name'] = row[4]
+                
+                # add user to root
+                from betahaus.pyracont import generate_slug
+                userid = row[0] = generate_slug(self.api.root, userid)
+                user = createContent('User', creators=[userid], **appstruct)
+                self.api.root.users[userid] = user
+                
+                # add user to meeting
+                self.context.add_groups(userid, roles, event = True)
+                
+                output.append(row)
+                participant_count = participant_count + 1
+                          
+            msg = _('added_participants_text', default=u"Successfully added ${participant_count} participants", mapping={'participant_count':participant_count} )
+            self.api.flash_messages.add(msg)
+            
+            self.response['count'] = participant_count
+            self.response['participants'] = output
+            return Response(render("templates/add_participants.pt", self.response, request = self.request))
+
+
+        #No action - Render add form
+        self.response['form'] = form.render()
         return self.response
